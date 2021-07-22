45642e12f4410fdd0b609ff86fd69137c63cfab2
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-22 22:11:59 +0700

    Try to use multiple frameworks

---
+++ src/components/SpeechButtonButSvelte.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let label
+  export let filename
+
+  const play = () => {
+    let sound = document.getElementById(filename)
+
+    sound.pause()
+    sound.currentTime = 0
+    sound.play()
+  }
+</script>
+
+<button
+  class="text-xl text-center px-4 py-2 rounded bg-red-500 m-4"
+  on:click={play}
+>
+  {label}
+</button>
+<audio id={filename} preload="metadata">
+  <source src={`/sounds/${filename}.mp3`} type="audio/mpeg" />
+</audio>
