e6059a8ff0a77ad940b4bfb39266002d93512b4d
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-25 14:28:23 +0700

    feat: Random speech button

---
+++ src/components/RandomSpeechButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let data
+
+  const play = () => {
+    const index = Math.floor(Math.random() * data.length);
+    const randomButtonFilename = data[index].filename;
+
+    let sound = document.getElementById(randomButtonFilename)
+
+    sound.pause()
+    sound.currentTime = 0
+    sound.play()
+  }
+</script>
+
+<button
+  class="text-xl text-center px-4 py-2 rounded bg-red-500 m-4"
+  on:click={play}
+>
+  Random
+</button>
